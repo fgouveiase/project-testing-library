e40719da1a4b4f3e5ac8bd41c89d030e64168e21
felipe <felipegouveia.bfr@hotmail.com>
2022-11-27 22:31:22 -0300

    req 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import FavoritePokemon from '../pages/FavoritePokemon';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <FavoritePokemon.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const withoutFavorite = screen.getByText(/no favorite pokémon found/i);
+    expect(withoutFavorite).toBeInTheDocument();
+  });
+});
